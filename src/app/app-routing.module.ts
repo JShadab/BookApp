99af1d2fcd6fa89f537005573cc32f12befe0a20
unknown <shadab105662@gmil.com>
2022-06-15 13:08:02 +0530

    Initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CompletedbooksComponent } from './completedbooks/completedbooks.component';
+const routes: Routes = [
+  {path:'', redirectTo:'signup', pathMatch:'full'},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:SignupComponent},
+  {path:'wishlist', component:WishlistComponent},
+  {path:'completedbooks', component:CompletedbooksComponent},
+  {path:'viewlist', component:StudentDashboardComponent},
+  {path:'dashboard', component:StudentDashboardComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
+import {HttpClientModule} from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CompletedbooksComponent } from './completedbooks/completedbooks.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentDashboardComponent,
+    LoginComponent,
+    SignupComponent,
+    WishlistComponent,
+    CompletedbooksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
